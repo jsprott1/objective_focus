fde55d1fa06d52dcb98be23aa2f276e72f046330
Jack Sprott <jacksprott@proton.me>
2025-09-09 12:50:42 +0200

    Fixed rounding

--- autofocus.py
+++ autofocus.py
@@ -7,36 +7,36 @@ def focus(camera, stage, scan_range, factor, max_iter, pos_bounds, error_on_no_t
     frames = []
     positions = []
     initial_pos = stage.get_motor_position("o")
-    stage.move_to("o", initial_pos - scan_range/2)
+    stage.move_to_rd("o", initial_pos - scan_range/2)
     frames.append(get_frame(camera))
     positions.append(stage.get_motor_position("o"))
     for i in range(int(2/factor)):
-        stage.move_to("o", stage.get_motor_position("o") + 1/2*scan_range*factor)
+        stage.move_to_rd("o", stage.get_motor_position("o") + 1/2*scan_range*factor)
         frames.append(get_frame(camera))
         positions.append(stage.get_motor_position("o"))
     contrast = contrast_metric(frames)
     peak = peak_type(contrast)
     if 0 == max_iter:
         if peak_in_range or peak == Peaks.CENTRE_PEAK or not error_on_no_trend:
-            stage.move_to("o", positions[np.argmax(contrast)])
+            stage.move_to_rd("o", positions[np.argmax(contrast)])
             return np.max(contrast)
         else:
             warn("Focus not found")
-            stage.move_to("o", positions[np.argmax(contrast)])
+            stage.move_to_rd("o", positions[np.argmax(contrast)])
             return np.max(contrast)
     elif peak_in_range or peak == Peaks.CENTRE_PEAK:
-        stage.move_to("o", positions[np.argmax(contrast)])
+        stage.move_to_rd("o", positions[np.argmax(contrast)])
         return focus(camera, stage, scan_range * factor, factor, max_iter - 1, pos_bounds - (stage.get_motor_position("o") - initial_position), error_on_no_trend = error_on_no_trend, peak_in_range=True)
     elif peak == Peaks.NO_PEAK: 
         if error_on_no_trend:
             warn("Focus not found")
-            stage.move_to("o", positions[np.argmax(contrast)])
+            stage.move_to_rd("o", positions[np.argmax(contrast)])
             return np.max(contrast)
         else:
-            stage.move_to("o", initial_pos)
+            stage.move_to_rd("o", initial_pos)
             return focus(camera, stage, scan_range / factor, factor, max_iter - 1, pos_bounds, error_on_no_trend = error_on_no_trend, peak_in_range = False)
     elif peak == Peaks.EDGE_PEAK:
-        stage.move_to("o", positions[np.argmax(contrast)])
+        stage.move_to_rd("o", positions[np.argmax(contrast)])
         return focus(camera, stage, scan_range, factor, max_iter - 1, pos_bounds - (stage.get_motor_position("o") - initial_position), error_on_no_trend = error_on_no_trend, peak_in_range = False)
     
 # frames is a 3d numpy array of frames (gets mapped to [0,1]. Returns mean square difference between each pixel and its neighbourhood for each frame.
